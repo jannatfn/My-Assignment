d62fd7ea96a711cdd1b9a3a68f5897af9a1f3c8c
Jannatul Ferdous Nishat <jannatulnsht@gmail.com>
2026-02-01 09:42:57 +0600

    Add array, loop & conditions

--- Form-Validation/script.js
+++ Form-Validation/script.js
@@ -9,5 +9,37 @@ document.getElementById("myForm").addEventListener("submit", function(e) {
 
   let error = document.getElementById("error");
   error.innerText = "";
-  
-}
+
+   let inputs = [name, email, phone, password, confirm];
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i] === "") {
+      error.innerText = "All fields are required";
+      return;
+    }
+
+    }
+
+  if (name.length < 3) {
+    error.innerText = "Name must be at least 3 characters";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    error.innerText = "Invalid email";
+    return;
+  }
+
+  if (password.length < 6) {
+    error.innerText = "Password must be at least 6 characters";
+    return;
+  }
+
+  if (password !== confirm) {
+    error.innerText = "Passwords do not match";
+    return;
+  }
+
+  alert("Form Submitted Successfully!");
+});
+
